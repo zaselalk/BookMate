fa416ee5ba67f7430b877d1d505db0feea73079d
Asela <43272744+zaselalk@users.noreply.github.com>
2023-03-07 12:51:16 +0530

    :bug: sigin in form validation bug fix

--- js/form.js
+++
@@ -1,8 +0,0 @@
-function myfun(){
-    var name1=document.getElementById("f_name").value;
-    var pwd1=document.getElementById("n_pwd").value;
-    if((name1.length==0) && (pwd1.length==0)){
-        alert("Empty");
-        
-    }
-}
---
+++ js/signin.js
@@ -0,0 +1,7 @@
+function myfun() {
+  var name1 = document.getElementById("f_name").value;
+  var pwd1 = document.getElementById("n_pwd").value;
+  if (name1.length == 0 || pwd1.length == 0) {
+    alert("Empty");
+  }
+}
