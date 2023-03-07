8d4480bb6e18a9df31e353a55c6c66b62ec278e0
Dilshan samarasinghe <113366721+samareDilshan@users.noreply.github.com>
2023-03-07 12:47:02 +0530

    form updated

---
+++ js/form.js
@@ -0,0 +1,8 @@
+function myfun(){
+    var name1=document.getElementById("f_name").value;
+    var pwd1=document.getElementById("n_pwd").value;
+    if((name1.length==0) && (pwd1.length==0)){
+        alert("Empty");
+        
+    }
+}
