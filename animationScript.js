ce2839d31feceb7534744d5e98355a29ee9db891
Asela <43272744+zaselalk@users.noreply.github.com>
2023-03-07 22:25:19 +0530

    :art: Code formatted

--- animationScript.js
+++ animationScript.js
@@ -1,15 +1,13 @@
-const observer = new IntersectionObserver((entries) =>{
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-    entries.forEach((entry) => {
-        console.log(entry);
-        if( entry.isIntersecting ){
-            entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove('show');
-
-        }
-    } );
-} );
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el) );
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
