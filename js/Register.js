b1328c9ca8f911541f08ec8433eb9e7426eb6090
Dilshan samarasinghe <113366721+samareDilshan@users.noreply.github.com>
2023-03-08 13:49:41 +0530

    :sparkles: register and signin page updated

---
+++ js/Register.js
@@ -0,0 +1,30 @@
+function register_f(){
+    var ema1=document.getElementById("ema").value;
+    var paw1=document.getElementById("pwd2").value;
+    var re1=document.getElementById("re_pwd").value;
+    var ag1=document.getElementById("n_age").value;
+    if(ema1.length==0 || paw1.length==0 || re1.length==0 || ag1.length==0){
+        alert("Box is empty")
+    }
+    else{
+        valid_f();
+        agg_valid();
+    }    
+}
+
+
+function valid_f(){
+    var paw1=document.getElementById("pwd2").value;
+    var re1=document.getElementById("re_pwd").value;
+    if(paw1!=re1){
+        alert("password is not same")
+    }
+}
+
+function agg_valid(){
+    var ag1=document.getElementById("n_age").value;
+    if(!/^[0-9]+$/.test(ag1)){
+        alert("Not valid");
+    }
+    
+}
